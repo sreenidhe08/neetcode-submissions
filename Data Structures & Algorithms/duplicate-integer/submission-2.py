e9e18ceb1c6fbd4c58f578653f8f58a4f3db3be8
sreenidhe <sreenidhe08@gmail.com>
2026-07-10 22:16:57 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+        def hasDuplicate(self, nums: List[int]) -> bool:
+                hashset={}
+                for i,num in enumerate(nums):
+                        if num in hashset:
+                                 return True
+                        hashset[num]=i
+                return False
